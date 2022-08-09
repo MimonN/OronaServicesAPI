b935576588c77475691148387612e98f7b60da03
MimonN <m.a.nedilsky@gmail.com>
2022-08-09 08:25:19 -0400

    working crud w/ image

--- OronaServicesAPI/Controllers/UploadController.cs
+++ OronaServicesAPI/Controllers/UploadController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Contracts;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http.Headers;
 
 namespace OronaServicesAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class UploadController : ControllerBase
     {
+        private readonly IRepositoryWrapper _repository;
+
+        public UploadController(IRepositoryWrapper repository)
+        {
+            _repository = repository;
+        }
+
         [HttpPost, DisableRequestSizeLimit]
         public async Task<IActionResult> Upload()
         {
@@ -17,14 +25,50 @@ namespace OronaServicesAPI.Controllers
             var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
             if (file.Length > 0)
             {
-                var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                var fullPath = Path.Combine(pathToSave, fileName);
-                var dbPath = Path.Combine(folderName, fileName);
+                var fileName = Guid.NewGuid().ToString();
+                var extension = Path.GetExtension(formCollection.Files.First().FileName);
+                var fullPath = Path.Combine(pathToSave, fileName + extension);
+                var dbPath = Path.Combine(folderName, fileName + extension);
                 using (var stream = new FileStream(fullPath, FileMode.Create))
                 {
                     file.CopyTo(stream);
                 }
                 return Ok(new {dbPath});
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("{id}"), DisableRequestSizeLimit]
+        public async Task<IActionResult> UpdateImage(int id)
+        {
+            var window = await _repository.Window.GetWindowByIdAsync(id);
+            if (window == null)
+            {
+                return NotFound();
+            }
+            var oldImage = window.ImgPath;
+            if (System.IO.File.Exists(oldImage))
+            {
+                System.IO.File.Delete(oldImage);
+            }
+            var formCollection = await Request.ReadFormAsync();
+            var file = formCollection.Files.First();
+            var folderName = Path.Combine("Resources", "Images");
+            var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+            if (file.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString();
+                var extension = Path.GetExtension(formCollection.Files.First().FileName);
+                var fullPath = Path.Combine(pathToSave, fileName + extension);
+                var dbPath = Path.Combine(folderName, fileName + extension);
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                return Ok(new { dbPath });
             }
             else
             {
--- OronaServicesAPI/Controllers/WindowController.cs
+++ OronaServicesAPI/Controllers/WindowController.cs
@@ -2,7 +2,6 @@
 using Contracts;
 using Entities.DataTransferObjects;
 using Entities.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace OronaServicesAPI.Controllers
@@ -25,101 +24,108 @@ namespace OronaServicesAPI.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllWindows()
         {
-                var windows = await _repository.Window.GetAllWindowsAsync();
-                _logger.LogInfo($"Returned all windows from database.");
+            var windows = await _repository.Window.GetAllWindowsAsync();
+            _logger.LogInfo($"Returned all windows from database.");
 
-                var windowsResult = _mapper.Map<IEnumerable<WindowDto>>(windows);
-                return Ok(windowsResult);
-            
+            var windowsResult = _mapper.Map<IEnumerable<WindowDto>>(windows);
+            return Ok(windowsResult);
+
         }
 
         [HttpGet("{id}", Name = "WindowById")]
         public async Task<IActionResult> GetWindowById(int id)
         {
-                var window = await _repository.Window.GetWindowByIdAsync(id);
-                if (window == null)
-                {
-                    _logger.LogError($"Window with id: {id}, hasn't been found in db.");
-                    return NotFound();
-                }
-                else
-                {
-                    _logger.LogInfo($"Returned window with id: {id}");
+            var window = await _repository.Window.GetWindowByIdAsync(id);
+            if (window == null)
+            {
+                _logger.LogError($"Window with id: {id}, hasn't been found in db.");
+                return NotFound();
+            }
+            else
+            {
+                _logger.LogInfo($"Returned window with id: {id}");
 
-                    var windowResult = _mapper.Map<WindowDto>(window);
-                    return Ok(windowResult);
-                }
+                var windowResult = _mapper.Map<WindowDto>(window);
+                return Ok(windowResult);
+            }
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateWindow([FromBody] WindowForCreationDto window)
         {
-                if (window == null)
-                {
-                    _logger.LogError("Window object sent from client is null.");
-                    return BadRequest("Window object is null");
-                }
+            if (window == null)
+            {
+                _logger.LogError("Window object sent from client is null.");
+                return BadRequest("Window object is null");
+            }
 
-                if (!ModelState.IsValid)
-                {
-                    _logger.LogError("Invalid window object sent from client.");
-                    return BadRequest("Invalid model object");
-                }
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError("Invalid window object sent from client.");
+                return BadRequest("Invalid model object");
+            }
 
-                var windowEntity = _mapper.Map<Window>(window);
+            var windowEntity = _mapper.Map<Window>(window);
 
-                _repository.Window.CreateWindow(windowEntity);
-                await _repository.SaveAsync();
+            _repository.Window.CreateWindow(windowEntity);
+            await _repository.SaveAsync();
 
-                var createdWindow = _mapper.Map<WindowDto>(windowEntity);
+            var createdWindow = _mapper.Map<WindowDto>(windowEntity);
 
-                return CreatedAtRoute("WindowById", new { id = createdWindow.Id }, createdWindow);
+            return CreatedAtRoute("WindowById", new { id = createdWindow.Id }, createdWindow);
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateWindow(int id, [FromBody] WindowForUpdateDto window)
         {
-                if (window == null)
-                {
-                    _logger.LogError("Window object sent from client is null.");
-                    return BadRequest("Window object is null");
-                }
+            if (window == null)
+            {
+                _logger.LogError("Window object sent from client is null.");
+                return BadRequest("Window object is null");
+            }
 
-                if (!ModelState.IsValid)
-                {
-                    _logger.LogError("Invalid window object sent from client.");
-                    return BadRequest("Invalid model object");
-                }
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError("Invalid window object sent from client.");
+                return BadRequest("Invalid model object");
+            }
 
-                var windowEntity = await _repository.Window.GetWindowByIdAsync(id);
-                if (windowEntity == null)
-                {
-                    _logger.LogError($"Window with id: {id}, hasn't been found in db.");
-                    return NotFound();
-                }
+            var windowEntity = await _repository.Window.GetWindowByIdAsync(id);
+            if (windowEntity == null)
+            {
+                _logger.LogError($"Window with id: {id}, hasn't been found in db.");
+                return NotFound();
+            }
 
-                _mapper.Map(window, windowEntity);
+            _mapper.Map(window, windowEntity);
 
-                _repository.Window.UpdateWindow(windowEntity);
-                await _repository.SaveAsync();
+            _repository.Window.UpdateWindow(windowEntity);
+            await _repository.SaveAsync();
 
-                return NoContent();
+            return NoContent();
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteWindow(int id)
         {
-                var window = await _repository.Window.GetWindowByIdAsync(id);
-                if (window == null)
-                {
-                    _logger.LogError($"Window with id: {id}, hasn't been found in db.");
-                    return NotFound();
-                }
+            var window = await _repository.Window.GetWindowByIdAsync(id);
+            if (window == null)
+            {
+                _logger.LogError($"Window with id: {id}, hasn't been found in db.");
+                return NotFound();
+            }
 
-                _repository.Window.DeleteWindow(window);
-                await _repository.SaveAsync();
+            
+            var oldImage = window.ImgPath;
+            if (System.IO.File.Exists(oldImage))
+            {
+                System.IO.File.Delete(oldImage);
+            }
 
-                return NoContent();
+            _repository.Window.DeleteWindow(window);
+            await _repository.SaveAsync();
+
+            return NoContent();
         }
     }
 }
